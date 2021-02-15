b9e2c25be6f6c6dad5ef6e700c3f80b71dfb870e
Denise <denise.ngkl@gmail.com>
2021-02-16 03:48:32 +0800

    Initial commit

---
+++ lib/subset-navigator.d.ts
@@ -0,0 +1,10 @@
+import * as React from 'react';
+declare type Pages = Record<string, React.ComponentType<ScreenProps>>;
+interface ScreenProps {
+    navigator: React.Component;
+}
+declare class SubsetNavigator {
+    static createSubsetNavigator(nameOfFirstPage: string, pages: Pages): any;
+}
+export default SubsetNavigator;
+export declare const createSubsetNavigator: typeof SubsetNavigator.createSubsetNavigator;
